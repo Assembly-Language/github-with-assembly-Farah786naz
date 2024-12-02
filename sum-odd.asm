f1f79e6fe7057af564ad45fdca277fe2724f5cf8
Farah786Naz <farahnaz8124@gmail.com>
2024-12-02 12:17:08 +0500

    second commit

--- sum-odd.asm
+++ sum-odd.asm
@@ -1,18 +1,45 @@
-include irvine32.inc
-.data
+include irvine32.inc 
+.data 
+array SWORD 11,8,3,4,6,9,1,2
+msg db "The sum of all odd numbers is: ",0
+msg1 db "The total odd numbers are: ",0
+var1 Dword 0;
+Totaleven byte 0;
+.code 
+main proc 
+        mov eax,0
+        mov ebx,0
+	 	mov esi,OFFSET array 
+	 	mov ecx,lengthof array 
+ 	 next: 
+        mov ax,WORD PTR [esi]
+	 	test ax,1                ;8000h=(8)16 = (1000)2
+                                     ;      (0)16 = (0000)2
+                                     ;      (0)16 = (0000)2
+                                     ;      (0)16 = (0000)2 
+	 	jz moveTonext               ; will move to next index if the zero flag is set(incase is there is even value)
 
-; array and other variables declaration here
+        
+        add bx,ax                   ;will only add the odd number incase the moveTonext doesnt work
+        inc Totaleven            ;will increment the counter if the counter if there is odd number
 
-.code
-main proc
+	 	 moveTonext:
+                        add esi,TYPE array  	   ;let this move to next index
+ 	                    loop next                  ;will continue the loop
 
+        mov edx,offset msg
+        call writestring
+        call crlf  
+        movzx eax,bx                              
+        mov var1,eax
+        call writeint
 
-;code here
-
-
-
-exit
+        call crlf
+        mov edx,offset msg1
+        call writestring
+        call crlf              
+        movzx eax,Totaleven                  
+        call writeint                
+        exit
 main endp
-
-
 end main
